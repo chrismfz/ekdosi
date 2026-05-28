3e4cd811e635e92546fa9f3d4da90744b7f22ef8
Chris <chrismfz@gmail.com>
2026-05-28 14:37:25 +0300

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,5 +1,7 @@
 sudo systemctl restart php-fpm
 sudo -u ekdosi  composer dump-autoload --optimize
+
+sudo -u ekdosi php artisan shield:generate
 sudo -u ekdosi php artisan migrate --force  
 sudo -u ekdosi php artisan optimize:clear
 sudo -u ekdosi php artisan cache:clear
@@ -8,4 +10,3 @@ sudo -u ekdosi php artisan route:clear
 sudo -u ekdosi php artisan view:clear
 sudo -u ekdosi php artisan optimize
 sudo -u ekdosi php artisan queue:restart
-
