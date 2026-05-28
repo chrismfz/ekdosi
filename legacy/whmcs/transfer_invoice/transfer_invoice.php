9b41caf98bc06a312b17f9ae4dc803636aa4ea25
Christos Panagiotakis <chrismfz@gmail.com>
2026-05-28 21:59:23 +0300

    whmcs plugins legacy

---
+++ legacy/whmcs/transfer_invoice/transfer_invoice.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * WHMCS SDK Sample Addon Module
+ *
+ * An addon module allows you to add additional functionality to WHMCS. It
+ * can provide both client and admin facing user interfaces, as well as
+ * utilise hook functionality within WHMCS.
+ *
+ * This sample file demonstrates how an addon module for WHMCS should be
+ * structured and exercises all supported functionality.
+ *
+ * Addon Modules are stored in the /modules/addons/ directory. The module
+ * name you choose must be unique, and should be all lowercase, containing
+ * only letters & numbers, always starting with a letter.
+ *
+ * Within the module itself, all functions must be prefixed with the module
+ * filename, followed by an underscore, and then the function name. For this
+ * example file, the filename is "addonmodule" and therefore all functions
+ * begin "addonmodule_".
+ *
+ * For more information, please refer to the online documentation.
+ *
+ * @see https://developers.whmcs.com/addon-modules/
+ *
+ * @copyright Copyright (c) WHMCS Limited 2017
+ * @license http://www.whmcs.com/license/ WHMCS Eula
+ */
+
+/**
+ * Require any libraries needed for the module to function.
+ * require_once __DIR__ . '/path/to/library/loader.php';
+ *
+ * Also, perform any initialization required by the service's library.
+ */
+
+use WHMCS\Database\Capsule;
+use WHMCS\Module\Addon\TransferInvoice\Admin\AdminDispatcher;
+//use WHMCS\Module\Addon\TransferInvoice\Client\ClientDispatcher;
+
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+
+/**
+ * Define addon module configuration parameters.
+ *
+ * Includes a number of required system fields including name, description,
+ * author, language and version.
+ *
+ * Also allows you to define any configuration parameters that should be
+ * presented to the user when activating and configuring the module. These
+ * values are then made available in all module function calls.
+ *
+ * Examples of each and their possible configuration parameters are provided in
+ * the fields parameter below.
+ *
+ * @return array
+ */
+function transfer_invoice_config()
+{
+    return [
+        // Display name for your module
+        'name' => 'Transfer invoice between clients',
+        // Description displayed within the admin interface
+        'description' => 'This module provides an example WHMCS Addon Module'
+            . ' which can be used as a basis for building a custom addon module.',
+        // Module author name
+        'author' => 'MyIP Networks',
+        // Default language
+        'language' => 'english',
+        // Version number
+        'version' => '1.0',
+        'fields' => [
+        ]
+    ];
+}
+
+/**
+ * Activate.
+ *
+ * Called upon activation of the module for the first time.
+ * Use this function to perform any database and schema modifications
+ * required by your module.
+ *
+ * This function is optional.
+ *
+ * @see https://developers.whmcs.com/advanced/db-interaction/
+ *
+ * @return array Optional success/failure message
+ */
+function transfer_invoice_activate()
+{
+    // Create custom tables and schema required by your module
+    try {
+        return [
+            // Supported values here include: success, error or info
+            'status' => 'success',
+            'description' => 'This is a demo module only. '
+                . 'In a real module you might report a success or instruct a '
+                    . 'user how to get started with it here.',
+        ];
+    } catch (\Exception $e) {
+        return [
+            // Supported values here include: success, error or info
+            'status' => "error",
+            'description' => 'Unable to create mod_addonexample: ' . $e->getMessage(),
+        ];
+    }
+}
+
+/**
+ * Deactivate.
+ *
+ * Called upon deactivation of the module.
+ * Use this function to undo any database and schema modifications
+ * performed by your module.
+ *
+ * This function is optional.
+ *
+ * @see https://developers.whmcs.com/advanced/db-interaction/
+ *
+ * @return array Optional success/failure message
+ */
+function atransfer_invoice_deactivate()
+{
+    // Undo any database and schema modifications made by your module here
+    try {
+       return [
+            // Supported values here include: success, error or info
+            'status' => 'success',
+            'description' => 'This is a demo module only. '
+                . 'In a real module you might report a success here.',
+        ];
+    } catch (\Exception $e) {
+        return [
+            // Supported values here include: success, error or info
+            "status" => "error",
+            "description" => "Unable to drop mod_addonexample: {$e->getMessage()}",
+        ];
+    }
+}
+
+
+function transfer_invoice_output($vars)
+{
+    // Get common module parameters
+    $modulelink = $vars['modulelink']; // eg. addonmodules.php?module=addonmodule
+    $version = $vars['version']; // eg. 1.0
+    $_lang = $vars['_lang']; // an array of the currently loaded language variables
+
+    // Get module configuration parameters
+
+    // Dispatch and handle request here. What follows is a demonstration of one
+    // possible way of handling this using a very basic dispatcher implementation.
+
+    $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
+
+    $dispatcher = new AdminDispatcher();
+    $response = $dispatcher->dispatch($action, $vars);
+    echo $response;
+}
